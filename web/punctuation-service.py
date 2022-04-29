2e5d3e1f3adbdcf4827dc44d6ceeed8ed8058682
Jordi Mas <jmas@softcatala.org>
2022-04-29 12:19:08 +0200

    Cache lines already calculated

--- web/punctuation-service.py
+++ web/punctuation-service.py
@@ -60,15 +60,33 @@ def health_api_get():
     health['inference_calls'] = inference_calls
     health['torch_num_threads'] = torch.get_num_threads()
     health['average_time_per_request'] = total_seconds / inference_calls if inference_calls else 0
+
+    total = hits + misses
+    phits = (hits * 100 / total) if total else 0
+
+    health['cache_misses'] = misses
+    health['cache_hits'] = f"{hits} ({phits:.2f}%)"
+    health['cache_size'] = len(g_cache)
     return health
 
 model = PunctuationModel(punctuation = ",")
 
 NEW_LINE = "\n"
 
+g_cache = {}
+g_cache_ttl = time.time()
+misses = 0
+hits = 0
+EXPIRE_CACHE = 30 * 60
+CACHE_ITEMS = 'CACHE_ITEMS'
+g_cached_enabled = (CACHE_ITEMS in os.environ and os.environ[CACHE_ITEMS].lower() == 'false') == False
+print(f"Sentence cache enabled {g_cached_enabled}")
+
 def _punctuation_api(values):
     try:
       global inference_calls, total_seconds
+      global misses, hits
+        
       start_time = datetime.datetime.now()
 
       inference_calls += 1
@@ -81,11 +99,26 @@ def _punctuation_api(values):
 
       for sentence in sentences:
         if len(sentence.strip()) > 0:
-          corrected_local = model.restore_punctuation(sentence)
-   #       print(f"Corrected: {corrected_local}")
-          corrected += corrected_local
-        if has_line:
-          corrected += NEW_LINE
+
+            if g_cached_enabled and text in g_cache:
+                hits += 1
+                corrected_local = g_cache[text]
+            else:
+                corrected_local = model.restore_punctuation(sentence)
+
+                if g_cached_enabled:
+                    global g_cache_ttl
+                    if time.time() > EXPIRE_CACHE + g_cache_ttl:
+                        g_cache.clear()
+                        g_cache_ttl = time.time()
+
+                    misses += 1
+                    g_cache[text] = corrected_local
+
+            corrected += corrected_local
+
+            if has_line:
+                corrected += NEW_LINE
 
       time_used = datetime.datetime.now() - start_time
       total_seconds += (time_used).total_seconds()
