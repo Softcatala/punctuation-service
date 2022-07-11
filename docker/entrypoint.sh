14373474611f73516a848fa36c7a42a3a6af7db4
Jordi Mas <jmas@softcatala.org>
2022-07-11 19:20:41 +0200

    More time to start services

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 cd srv/web/
-gunicorn punctuation-service:app -b 0.0.0.0:8000 --workers=1
+gunicorn  --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 punctuation-service:app -b 0.0.0.0:8000
