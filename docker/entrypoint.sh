7666ace66ea8ddb31a309a5532df925e574b4f77
Jordi Mas <jmas@softcatala.org>
2022-07-27 08:09:19 +0200

    gunicron 1 worker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 cd srv/web/
-gunicorn  --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 punctuation-service:app -b 0.0.0.0:8000
+# if workers > 1 cache will not b shared across workers
+gunicorn  --workers=1 --graceful-timeout 90 --timeout 90 --threads=8 punctuation-service:app -b 0.0.0.0:8000
