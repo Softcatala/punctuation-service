fefe78295cbe34a30ec91522bd6803eb9a7ddc75
jaumeortola <jaumeortola@gmail.com>
2025-02-23 12:44:54 +0100

    2 workers

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 cd srv/web/
 # if workers > 1 cache will not b shared across workers
-uvicorn punctuation-service:app --host 0.0.0.0 --port 8000 --workers 1 --timeout-keep-alive 90 --loop "uvloop"
+uvicorn punctuation-service:app --host 0.0.0.0 --port 8000 --workers 2 --timeout-keep-alive 90 --loop "uvloop"
