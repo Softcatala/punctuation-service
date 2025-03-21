3485b67a091de18897f2bf8c54850bbf1172a21f
Jordi Mas <jmas@softcatala.org>
2025-03-21 11:05:07 +0100

    Add logging

--- web/punctuation-service.py
+++ web/punctuation-service.py
@@ -8,7 +8,8 @@ from collections import OrderedDict
 import datetime
 import os
 import psutil
-#TODO: health, log
+import logging
+import logging.handlers
 
 calls = 0
 total_seconds = 0
@@ -19,11 +20,32 @@ app = FastAPI()
 model_name = "model"
 tokenizer = MT5Tokenizer.from_pretrained(model_name)
 
+def init_logging():
+    LOGDIR = os.environ.get("LOGDIR", "")
+    LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+    logger = logging.getLogger()
+    logfile = os.path.join(LOGDIR, "puntuation-service.log")
+    hdlr = logging.handlers.RotatingFileHandler(
+        logfile, maxBytes=1024 * 1024, backupCount=1
+    )
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(LOGLEVEL)
+
+    console = logging.StreamHandler()
+    console.setLevel(LOGLEVEL)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+
+init_logging()
+
 def get_model():
     inter_threads = int(os.environ.get('CTRANSLATE_INTER_THREADS', 2))
     intra_threads = int(os.environ.get('CTRANSLATE_INTRA_THREADS', 6))
     device = os.environ.get('DEVICE', "cpu")
-    print(f"device: {device}, inter_threads: {inter_threads}, intra_threads: {intra_threads}")
+    logging.info(f"device: {device}, inter_threads: {inter_threads}, intra_threads: {intra_threads}")
     return ctranslate2.Translator(model_name, device="cpu", inter_threads=inter_threads, intra_threads=intra_threads)
 
 model = get_model()
@@ -65,6 +87,7 @@ def process_sentences(sentences: list[str]) -> list[str]:
     uncached_sentences = cache.get_uncached(sentences)
     uncached_sentences_corrected = []
     if uncached_sentences:
+        cached = False
         inputs = [tokenizer.convert_ids_to_tokens(tokenizer.encode(sentence, add_special_tokens=True)) for sentence in uncached_sentences]
         results = model.translate_batch(inputs, max_decoding_length=300, batch_type="examples", beam_size=2, use_vmap=True)
         # , max_batch_size=64, beam_size=4 (més precisió). beam_size=1 (més ràpid)
@@ -75,12 +98,15 @@ def process_sentences(sentences: list[str]) -> list[str]:
             cache.put(uncached_sentences[i], uncached_sentences_corrected[i])
     else:
         cached_sentences += 1
+        cached = True
+
     output_sentences = [cache.get(s) for s in sentences]
     time_used = datetime.datetime.now() - start_time
 
     total_seconds += (time_used).total_seconds()
     words = sum(len(s.split()) for s in sentences)
     total_words += words
+    logging.debug(f"Words {words}, time {time_used}, cached {cached}")
     return {"output_sentences": output_sentences, "time": str(time_used)}
 
 # L'entrada del mètode POST és una llista de frases. 
@@ -110,4 +136,4 @@ def health_get():
     health['calls'] = calls
     health['cached_sentences'] = cached_sentences
     health['words_per_second'] = total_words / total_seconds if total_seconds else 0
-    return health       
+    return health
