7870ec27d650db2d3704e83b7839aab28f9671c5
Jordi Mas <jmas@softcatala.org>
2025-11-13 19:05:41 +0100

    type + workers

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,4 @@
 cd srv/web/
 # if workers > 1 cache will not b shared across workers
 rm -f $LOGDIR/id.txt
-uvicorn punctuation-service:app --host 0.0.0.0 --port 8000 --workers 8 --timeout-keep-alive 90 --loop "uvloop"
+uvicorn punctuation-service:app --host 0.0.0.0 --port 8000 --workers 4 --timeout-keep-alive 90 --loop "uvloop"
